b9d9ab05ee0694a54334b3155231ee3589ee59f8
bir.virk90@gmail.com <jattsaab90>
2015-03-20 18:25:09 -0400

    Basic Layout, Design

---
+++ Scripts/objects/plane.js
@@ -0,0 +1,27 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var objects;
+(function (objects) {
+    // PLANE CLASS
+    var Plane = (function (_super) {
+        __extends(Plane, _super);
+        // CONSTRUCTOR
+        function Plane() {
+            _super.call(this, assets.getResult("plane"));
+            this.y = 430;
+            this.regX = this.getBounds().width * 0.5;
+            this.regY = this.getBounds().height * 0.5;
+        }
+        // PUBLIC METHODS
+        Plane.prototype.update = function () {
+            this.x = stage.mouseX;
+        };
+        return Plane;
+    })(createjs.Bitmap);
+    objects.Plane = Plane;
+})(objects || (objects = {}));
+//# sourceMappingURL=plane.js.map
---
+++ Scripts/objects/plane.ts
@@ -0,0 +1,22 @@
+﻿
+module objects {
+    // PLANE CLASS
+    export class Plane extends createjs.Bitmap {
+        // CONSTRUCTOR
+        constructor() {
+            super(assets.getResult("plane"));
+
+            this.y = 430;
+            this.regX = this.getBounds().width * 0.5;
+            this.regY = this.getBounds().height * 0.5;
+
+        }
+
+        // PUBLIC METHODS
+        public update() {
+            this.x = stage.mouseX;
+        }
+
+    }
+
+} 
